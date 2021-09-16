8739758de5122690b692ba3572c9528d0fa199fc
bynull <xrw.null@gmail.com>
2021-09-15 20:09:17 -0700

    test: validator

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,54 @@
-use openssl::sha;
-use std::borrow::Borrow;
+use openssl::sha::sha256;
 
 fn main() {
-    use openssl::sha::sha256;
+    let difficulty = 7;
 
     let mut counter = 0;
+
     let mut hash;
+    let mut hash_str;
+
     loop {
+        if counter % 100_000 == 0 {
+            println!("iteration: {}", counter)
+        }
+
         counter = counter + 1;
 
         hash = sha256(counter.to_string().as_bytes());
-        let hash_str = hex::encode(hash);
-        //println!("{}", hash_str);
+        hash_str = hex::encode(hash);
 
-        let first_letter = hash_str.chars().next().unwrap();
-        //println!("{}", first_letter);
+        let is_valid = is_valid(difficulty, &hash_str);
 
-        if first_letter == '0' {
+        if is_valid {
             break;
         }
     }
-    println!("hash = {}", hex::encode(hash));
 
+    println!("counter: {}, hash: {}", counter, hash_str);
+}
 
+fn is_valid(difficulty: usize, hash: &String) -> bool {
+    let mut chars = hash.chars();
+    let mut accepted = true;
+    for i in 0..difficulty {
+        let curr_letter = chars.next().unwrap();
+
+        if curr_letter != '0' {
+            accepted = false;
+            break;
+        }
+    }
+    accepted
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::is_valid;
+
+    #[test]
+    fn test_is_valid() {
+        let actual_result = is_valid(2, &String::from("00123"));
+        assert_eq!(actual_result, true);
+    }
 }
